ad0c41b87279e4efc7bef34e14449301b77fc90e
Kobus Joubert <kobus@translate3d.com>
2014-06-28 21:04:26 +0200

    Amazon buckets for each environment

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
-Paperclip::Attachment.default_options[:bucket] = 'player1herokuapp'
+# Paperclip::Attachment.default_options[:bucket] = 'player1herokuapp'
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = ':attachment/:id/:style.:extension'
 Paperclip::Attachment.default_options[:default_url] = 'default/:attachment/:style/missing.png'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
