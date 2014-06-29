08e44e9b3f8cc26982d539cf0148d7794d3ce6dc
Kobus Joubert <kobus@translate3d.com>
2014-06-29 22:36:40 +0200

    Public latest artists view order set from controller instead of the view, I mean, common!

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,9 @@
 class PublicController < ApplicationController
   skip_before_action :authenticate_user!
 
+  # GET /albums
+  # GET /albums.json
   def index
+    @albums = Album.page(params[:page]).per(10).order(created_at: :desc)
   end
 end
