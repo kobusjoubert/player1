f1b6e2cd73f48b29e42361c0fef0b486ede021a2
Kobus Joubert <kobus@translate3d.com>
2014-06-29 20:18:47 +0200

    Before action added to tracks, set album

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,5 +1,6 @@
 class TracksController < ApplicationController
   before_action :set_track, only: [:show, :edit, :update, :destroy]
+  before_action :set_ablum
 
   # GET /albums/1/tracks
   # GET /albums/1/tracks.json
@@ -70,5 +71,9 @@ class TracksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def track_params
       params.require(:track).permit(:name, :duration)
+    end
+
+    def set_ablum
+      @album = Album.find(params[:album_id])
     end
 end
