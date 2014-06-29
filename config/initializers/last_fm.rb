18433ad670358aef504636e79725af24dbf3e7b3
Kobus Joubert <kobus@translate3d.com>
2014-06-29 12:46:32 +0200

    Argh, Last FM API key added as environment variable locally instead

--- config/initializers/last_fm.rb
+++ config/initializers/last_fm.rb
@@ -1,6 +1,5 @@
 # Set LAST_FM_API_KEY environment variable for production
 
 if Rails.env != "production"
-  LAST_FM = YAML.load_file("#{Rails.root}/config/last_fm.yml")[Rails.env]
-  LAST_FM_API_KEY = LAST_FM["api_key"]
+  ENV["LAST_FM_API_KEY"] = YAML.load_file("#{Rails.root}/config/last_fm.yml")[Rails.env]["api_key"]
 end
