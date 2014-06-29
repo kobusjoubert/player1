c1a44945bf0d2fc72ba8c88280e3ae3734564c22
Kobus Joubert <kobus@translate3d.com>
2014-06-29 21:40:38 +0200

    Dependent destroy added to album to destroy all tracks on album deletion

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ActiveRecord::Base
   has_many :album_artists, dependent: :destroy
   has_many :artists, through: :album_artists
-  has_many :tracks
+  has_many :tracks, dependent: :destroy
   
   enum genre: [:not_set, :blues, :classical, :country, :electronic, :folk, :jazz, :new_age, :reggae, :rock]
 
